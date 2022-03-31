35b52398d808dbd56d3a1e65a57fe0869e39a096
Asanda <noelmajola@gmail.com>
2022-03-31 23:17:17 +0200

    updated file

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function colorChange(elm){
 function greet(){
     return "Hello PetrolHead!";
 }
+//This will return an array of Html elements since the 'bi-geo-fill' class is associated with more than one element.
+let featuredListings=document.getElementsByClassName('bi-geo-fill');
+console.log(featuredListings)
+
+//Manipulating an achor tag attribute in html using 'setAttribute' method.
+let carValuatorLink=document.getElementById("scriptOne");
+carValuatorLink.setAttribute("href","https://www.autotrader.co.za/car-valuation");
+carValuatorLink.setAttribute("target","_blank");
+
+let financeCalculatorLink=document.getElementById("scriptTwo");
+financeCalculatorLink.setAttribute("href","https://www.wesbank.co.za/home/calculate");
+financeCalculatorLink.setAttribute("target","_blank");
+
+let carFinanceLink=document.getElementById("scriptThree");
+carFinanceLink.setAttribute("href","https://www.nedbank.co.za/content/nedbank/desktop/gt/en/personal/loans/vehicle-finance-mfc.html");
+carFinanceLink.setAttribute("target","_blank");
+
+let carInsuranceLink=document.getElementById("scriptFour");
+carInsuranceLink.setAttribute("href","https://www.oldmutualinsurance.co.za/products/car-insurance?UCID=OMSTIPERFMAX0001&gclid=CjwKCAjwopWSBhB6EiwAjxmqDTfXzUVD1uXL4vE5RSMv2YiK4BoTlaHGwOiXgJ0x6OteMi7hlzc8bRoCM04QAvD_BwE");
+carInsuranceLink.setAttribute("target","_blank");
+
+let carVehicleCheckLink=document.getElementById("scriptFive");
+carVehicleCheckLink.setAttribute("href","https://www.vehiclecheck.co.za/");
+carVehicleCheckLink.setAttribute("target","_blank");
